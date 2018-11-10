7445af525692cc6d8f8dcbf13c0e8b20736777e3
Qasim <q.mumtaz@hotmail.com>
2018-11-10 12:48:28 +0000

    initialCommit

---
+++ DatingApp.API/Data/Context.cs
@@ -0,0 +1,14 @@
+namespace DatingApp.API.Data
+{
+    using DatingApp.API.Models;
+    using Microsoft.EntityFrameworkCore;
+
+public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {   
+        }
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
